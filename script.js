8e0c58caeabc719e711108fabe72da26f8f98f25
youngjerald239 <youngjerald239@gmail.com>
2022-09-07 16:59:53 -0500

    set up win condition

--- script.js
+++ script.js
@@ -123,17 +123,41 @@ window.addEventListener('load', function() {
             this.game = game
             this.fontSize = 25
             this.fontFamily = 'Helvetica'
-            this.color = 'yellow'
+            this.color = 'white'
         }
         draw(context){
+            context.save()
+            context.fillStyle = this.color
+            context.shadowOffsetX = 2
+            context.shadowOffsetY = 2
+            context.shadowColor = 'black'
             context.font = this.fontSize + 'px ' + this.fontFamily
             // score
             context.fillText('score: ' + this.game.score, 20, 40)
             //ammo
-            context.fillStyle = this.color
+            
             for (let i = 0; i < this.game.ammo; i++){
                 context.fillRect(20 + 5 * i, 50, 3, 20)
             }
+            // game over message
+            if (this.game.gameOver){
+                context.textAlign = 'center'
+                let message1
+                let message2
+                if (this.game.score > this.game.winningScore){
+                    message1 = 'You win!'
+                    message2 = 'Well Done!'
+                } else {
+                    message1 = 'You Lose!'
+                    message2 = 'Try again next time!'
+
+                }
+                context.font = '50px ' + this.fontFamily
+                context.fillText(message1, this.game.width * 0.5, this.game.height * 0.5 - 40)
+                context.font = '25px ' + this.fontFamily
+                context.fillText(message2, this.game.width * 0.5, this.game.height * 0.5 + 40)
+            }
+            context.restore()
         }
     }
     class Game {
