aad803aa55f05be97844a857b6b638bd88182cca
youngjerald239 <youngjerald239@gmail.com>
2022-09-08 12:01:40 -0500

    added verticle boundaries

--- script.js
+++ script.js
@@ -71,6 +71,9 @@ window.addEventListener('load', function() {
             else if (this.game.keys.includes('s')) this.speedY = this.maxSpeed
             else this.speedY = 0
             this.y += this.speedY
+            // verticle boundaries
+            if (this.y > this.game.height - this.height * 0.5) this.y = this.game.height - this.height * 0.5
+            else if (this.y < -this.height * 0.5) this.y = -this.height * 0.5
             // handle projectiles
             this.projectiles.forEach(projectile => {
                 projectile.update()
@@ -146,8 +149,11 @@ window.addEventListener('load', function() {
         draw(context){
             if (this.game.debug) context.strokeRect(this.x, this.y, this.width, this.height)
             context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height)
-            context.font = '20px Helvetica'
-            context.fillText(this.lives, this.x, this.y)
+            if (this.game.debug){
+                context.font = '20px Helvetica'
+                context.fillText(this.lives, this.x, this.y)
+            }
+            
         }
     }
     class Angler1 extends Enemy {
@@ -231,7 +237,7 @@ window.addEventListener('load', function() {
         constructor(game){
             this.game = game
             this.fontSize = 25
-            this.fontFamily = 'Helvetica'
+            this.fontFamily = 'Bangers'
             this.color = 'white'
         }
         draw(context){
@@ -253,13 +259,14 @@ window.addEventListener('load', function() {
                 let message2
                 if (this.game.score > this.game.winningScore){
                     message1 = 'You win!'
-                    message2 = 'Well Done!'
+                    message2 = 'Well Done Explorer!'
                 } else {
                     message1 = 'You Lose!'
-                    message2 = 'Try again next time!'
+                    message2 = 'Ship Destroyed! Time for repairs...'
 
                 }
-                context.font = '50px ' + this.fontFamily
+                context.fillStyle = 'gold'
+                context.font = '100px ' + this.fontFamily
                 context.fillText(message1, this.game.width * 0.5, this.game.height * 0.5 - 40)
                 context.font = '25px ' + this.fontFamily
                 context.fillText(message2, this.game.width * 0.5, this.game.height * 0.5 + 40)
